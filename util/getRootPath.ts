469ff86f25a6af61d8bb58bd775c9085ba22c689
cklwblove <lwbhtml@163.com>
2022-10-26 16:25:28 +0800

    feat: 🎸 add getRootPath

---
+++ util/getRootPath.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+
+/**
+ * Get user root directory
+ * @param dir file path
+ */
+export function getRootPath(...dir: string[]) {
+  return path.resolve(process.cwd(), ...dir);
+}
