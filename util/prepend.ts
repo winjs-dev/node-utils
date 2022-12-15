f8016103ab5fef2bda81c44d10677c41c0e6328c
cklwblove <lwbhtml@163.com>
2022-12-15 13:36:22 +0800

    feat(prepend): 添加 prepend 函数

---
+++ util/prepend.ts
@@ -0,0 +1,6 @@
+import fse from 'fs-extra';
+
+export async function prepend(file, string) {
+  const data = await fse.readFile(file, 'utf8');
+  await fse.writeFile(file, string + data, 'utf8');
+}
