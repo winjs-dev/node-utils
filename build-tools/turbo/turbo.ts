7f36481b094c6425a7b3dbebaa93a37aae5182ac
cklwblove <lwbhtml@163.com>
2023-05-05 14:42:18 +0800

    feat: 🎸 增加 turbo

---
+++ build-tools/turbo/turbo.ts
@@ -0,0 +1,73 @@
+/**
+ * turbo
+ * @Author: liwb (lwbhtml@163.com)
+ * @Date: 2023-05-05 14:40
+ * @LastEditTime: 2023-05-05 14:40
+ * @Description: turbo
+ */
+import * as logger from '@umijs/utils/dist/logger';
+import spawn from '@umijs/utils/compiled/cross-spawn';
+import yArgs from '@umijs/utils/compiled/yargs-parser';
+import { join } from 'path';
+
+(async () => {
+  const args = yArgs(process.argv.slice(2));
+  const scope = args.scope || '!@example/*';
+  const extra = (args._ || []).join(' ');
+
+  await turbo({
+    cmd: args.cmd,
+    scope,
+    extra,
+    cache: args.cache,
+    parallel: args.parallel,
+  });
+})();
+
+/**
+ * Why not use zx ?
+ *  - `zx` not support color stdin on subprocess
+ *  - see https://github.com/google/zx/blob/main/docs/known-issues.md#colors-in-subprocess
+ *        https://github.com/google/zx/issues/212
+ */
+async function cmd(command: string) {
+  const result = spawn.sync(command, {
+    stdio: 'inherit',
+    shell: true,
+    cwd: join(__dirname, '../'),
+  });
+  if (result.status !== 0) {
+    // sub package command don't stop when execute fail.
+    // display exit
+    logger.error(`Execute command error (${command})`);
+    process.exit(1);
+  }
+  return result;
+}
+
+async function turbo(opts: {
+  scope: string;
+  cmd: string;
+  extra?: string;
+  cache?: boolean;
+  parallel?: boolean;
+}) {
+  const extraCmd = opts.extra ? `-- -- ${opts.extra}` : '';
+  const cacheCmd = opts.cache === false ? '--no-cache --force' : '';
+  const parallelCmd = opts.parallel ? '--parallel' : '';
+
+  const options = [
+    opts.cmd,
+    `--cache-dir=".turbo"`,
+    `--scope="${opts.scope}"`,
+    `--no-deps`,
+    `--include-dependencies`,
+    cacheCmd,
+    parallelCmd,
+    extraCmd,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return cmd(`turbo run ${options}`);
+}
